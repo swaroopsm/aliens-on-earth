d8fdaa65b41dc07eaf8fdcb3f58d6f60d1045b33
Swaroop SM <swaroop.striker@gmail.com>
2014-12-18 20:58:05 +0530

    added comments in utilities

--- lib/aliens_on_earth/utilities.rb
+++ lib/aliens_on_earth/utilities.rb
@@ -1,10 +1,14 @@
 module AliensOnEarth
   module Utilities
 
+    # Return a string that is classized
+    # Eg.: text_renderer => TextRenderer
     def to_class(str)
       return str.split("_").map{ |split| split.capitalize }.join
     end
 
+    # Return a string that is humanized renderer
+    # Eg.: text_renderer => Text
     def humanize_renderer(str)
        str.split("_").map{ |split| split.capitalize }[0...-1].join(" ")
     end
