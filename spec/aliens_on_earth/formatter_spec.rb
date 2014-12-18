bf31d31072f401acb09c7713ae86612cdf05df60
Swaroop SM <swaroop.striker@gmail.com>
2014-12-18 21:11:26 +0530

    updated formatter testcase

--- spec/aliens_on_earth/formatter_spec.rb
+++ spec/aliens_on_earth/formatter_spec.rb
@@ -3,10 +3,11 @@ module AliensOnEarth
   describe Formatter do
 
     it "should return all the various renderer engines / formatters" do
-      hash = {
-        '1' => { :basename => 'pdf_renderer', :classname => 'PdfRenderer', :title => 'Pdf' },
-        '2' => { :basename => 'text_renderer', :classname => 'TextRenderer', :title => 'Text' }
-      }
+      hash = {}
+      Dir.glob(File.expand_path('../../../lib/aliens_on_earth/renderers/*', __FILE__)).each_with_index do |renderer, index|
+        basename = File.basename(renderer, '.rb')
+        hash[(index+1).to_s]  = { :basename => basename, :classname => basename.split("_").map{ |r| r.capitalize }.join, :title => basename.split("_").map{ |r| r.capitalize }[0...-1].join }
+      end
 
       expect(Formatter.all).to eq(hash)
     end
